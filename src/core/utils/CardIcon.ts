e5f3ac2247ffd58992695a7d61f1fab6758eee0a
Marcelo Macedo <marcelo3macedo@gmail.com>
2026-01-26 13:58:21 -0300

    feat: type of cards

---
+++ src/core/utils/CardIcon.ts
@@ -0,0 +1,18 @@
+import iconIce from '@/assets/icons/attribute_ice.jpg';
+import iconDark from '@/assets/icons/attribute_dark.jpg';
+import iconFire from '@/assets/icons/attribute_fire.jpg';
+import iconSpell from '@/assets/icons/attribute_spell.jpg';
+
+export type AttributeType = 'ice' | 'dark' | 'fire' | 'spell' | 'trap';
+
+export const getAttributeIcon = (type: string | AttributeType): string => {
+  const icons: Record<string, string> = {
+    ice: iconIce,
+    dark: iconDark,
+    fire: iconFire,
+    spell: iconSpell,
+    default: iconDark 
+  };
+
+  return icons[type] || icons.default;
+};
