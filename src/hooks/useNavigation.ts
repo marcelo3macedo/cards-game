58912a4a0515da4aabcb88f1d08fd56160284f2e
Marcelo Macedo <marcelo3macedo@gmail.com>
2026-02-25 15:00:12 -0300

    feat: build fix

--- src/hooks/useNavigation.ts
+++ src/hooks/useNavigation.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import { authService } from "../services/authService";
 import { useNavigationStore } from "../store/NavigationStore";
 
