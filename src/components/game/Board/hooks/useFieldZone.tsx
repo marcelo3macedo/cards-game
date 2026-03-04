2c2528f9ec333f2e32416b79044ce80fd88cf4f2
Marcelo Macedo <marcelo3macedo@gmail.com>
2026-03-04 19:27:37 -0300

    fix: redirect to rewards correctly

--- src/components/game/Board/hooks/useFieldZone.tsx
+++ src/components/game/Board/hooks/useFieldZone.tsx
@@ -3,13 +3,30 @@ import { withContextLogging } from "../../../../utils/loggingUtils"
 import { useBattleStore } from "../../../../store/BattleStore";
 import { useBattleEventStore } from "../../../../store/BattleEventStore";
 import { BattleEvent } from "../../../../core/domain/BattleStore";
-import { useBattleEvents } from "../../../../scenarios/Battle/hooks/useBattleEvents";
+import { battleService } from "../../../../services/battleService";
+import { mapServerCardToEntity } from "../../../../utils/cardUtils";
 
 export const useFieldZone = ({ position, isMonster, isInteractable }: any) => {
-    const { event, setEvent } = useBattleStore();
-    const { setSelectedFieldIndex, setSelectedTargetIndex, setSelectedFieldArea, selectedAttackerIndex } = useBattleEventStore();
-    const { handleAttack } = useBattleEvents({});
+    const { event, setEvent, player, opponent } = useBattleStore();
+    const { setSelectedFieldIndex, setSelectedTargetIndex, setSelectedFieldArea, selectedAttackerIndex, setBattleData } = useBattleEventStore();
     const log = withContextLogging('useFieldZone');
+
+    const handleAttack = async ({ attackerIdx, targetIdx }: any) => {
+        try {
+            setEvent(BattleEvent.INITIAL);
+            const response = await battleService.attack(attackerIdx, targetIdx);
+            setBattleData({
+                attacker: mapServerCardToEntity(player?.field[attackerIdx]?.card),
+                defender: mapServerCardToEntity(opponent?.field[targetIdx]?.card),
+                position: opponent?.field[targetIdx]?.position
+            });
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            useBattleStore.getState().setPlayer(response.state.player);
+            useBattleStore.getState().setOpponent(response.state.opponent);
+        } catch (error: any) {
+            console.error("Erro ao realizar ataque:", error.message);
+        }
+    };
 
     const [showMenu, setShowMenu] = useState(false);
     const isFaceDown = (position === "face-down-attack" || position === "face-down-defense");
